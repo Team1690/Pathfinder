0eb1e65e39934ea006698d2a38d86205dc7e3b2b
Noam Prag <noam.prag0@gmail.com>
2021-10-21 19:49:45 +0300

    Added evaluation calculation for CubicBezier

--- lib/path_editor/cubic_bezier.dart
+++ lib/path_editor/cubic_bezier.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class CubicBezier {
   final Offset start;
@@ -6,12 +7,35 @@ class CubicBezier {
   final Offset endControl;
   final Offset end;
 
+  final List<Offset> pointsList;
+
   CubicBezier({
     required this.start,
     required this.startControl,
     required this.endControl,
     required this.end,
-  });
+  }) : pointsList = [start, startControl, endControl, end];
+
+  Offset evaluate(final double t) {
+    final double oneMinusT = 1 - t;
+
+    double oneMinusTFactor = pow(1 - t, 3).toDouble();
+    double tFactor = 1;
+
+    var result = Offset(0, 0);
+
+    for (int i = 0; i < 4; i++) {
+      result += pointsList[i] *
+          tFactor *
+          oneMinusTFactor *
+          (i == 1 || i == 2 ? 3 : 1);
+
+      oneMinusTFactor /= oneMinusT;
+      tFactor *= t;
+    }
+
+    return result;
+  }
 
   bool operator ==(final Object other) =>
       other is CubicBezier &&
