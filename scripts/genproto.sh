cee667c2025707ac2b7530f1960365f6d73ec6b3
Omri Kornblau <omri.354@gmail.com>
2021-11-18 10:08:50 +0200

    Add genproto and partial install scripts

---
+++ scripts/genproto.sh
@@ -0,0 +1,23 @@
+PROTOS_DIR="./protos"
+
+# GO Stuff
+GO_PACKAGE_DIR="./algorithm"
+GO_RPC_OUT_DIR="$GO_PACKAGE_DIR/rpc"
+
+# DART Stuff
+DART_RPC_OUT_DIR="./lib/rpc"
+
+# TODO: move the plugins to a directory here
+export PATH="$PATH:$HOME/AppData/Local/Pub/Cache/bin"
+export PATH="$PATH:$(go env GOPATH)/bin"
+
+for FILE in $PROTOS_DIR/*; do
+    protoc --proto_path=protos --go_out=$GO_PACKAGE_DIR \
+        --go-grpc_out=$GO_RPC_OUT_DIR --go-grpc_opt=paths=source_relative \
+        $FILE
+    echo "Generated GO code for '$FILE'"
+
+    protoc --dart_out=grpc:$DART_RPC_OUT_DIR \
+        $FILE
+    echo "Generated DART code for '$FILE'"
+done
