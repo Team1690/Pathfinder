9a458d56187de2943e5107745c7f8e85cb23a1ec
Noam Prag <noam.prag0@gmail.com>
2021-12-04 21:52:25 +0200

    Graphs for trajectory calculation via server

--- algorithm/main_test.go
+++ algorithm/main_test.go
@@ -2,22 +2,31 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"testing"
 
 	"github.com/Team1690/Pathfinder/rpc"
+	"github.com/Team1690/Pathfinder/utils/plot"
+	"github.com/Team1690/Pathfinder/utils/vector"
 )
 
 func Test(_ *testing.T) {
 	var (
+		chester = &rpc.TrajectoryRequest_SwerveRobotParams{
+			Width:           float32(0.3),
+			Height:          float32(0.3),
+			MaxVelocity:     3.83,
+			MaxAcceleration: 9,
+			MaxJerk:         50,
+		}
 		firstSegment = &rpc.Segment{
 			SplineType:  rpc.SplineTypes_Bezier,
 			MaxVelocity: 3.8,
 			Points: []*rpc.Point{
 				{
-					Position:   &rpc.Vector{X: 0.1, Y: 0.1},
+					Position:   &rpc.Vector{X: 0, Y: 0},
 					ControlOut: &rpc.Vector{X: 0, Y: 1},
 					Heading:    0,
 					UseHeading: true,
@@ -25,6 +34,13 @@ func Test(_ *testing.T) {
 				{
 					Position:   &rpc.Vector{X: 1, Y: 1},
 					ControlIn:  &rpc.Vector{X: 2, Y: 2},
+					ControlOut: &rpc.Vector{X: 0, Y: 0},
+					Heading:    math.Pi / 2,
+					UseHeading: true,
+				},
+				{
+					Position:   &rpc.Vector{X: 1, Y: 0},
+					ControlIn:  &rpc.Vector{X: 2, Y: 0},
 					Heading:    0,
 					UseHeading: true,
 				},
@@ -38,14 +54,8 @@ func Test(_ *testing.T) {
 	grpcServer := NewServer()
 
 	res, err := grpcServer.CalculateTrajectory(context.TODO(), &rpc.TrajectoryRequest{
-		SwerveRobotParams: &rpc.TrajectoryRequest_SwerveRobotParams{
-			Width:           float32(0.3),
-			Height:          float32(0.3),
-			MaxVelocity:     3.83,
-			MaxAcceleration: 9,
-			MaxJerk:         50,
-		},
-		Sections: []*rpc.Section{&section},
+		SwerveRobotParams: chester,
+		Sections:          []*rpc.Section{&section},
 	})
 	if err != nil {
 		if err != nil {
@@ -53,76 +63,65 @@ func Test(_ *testing.T) {
 		}
 	}
 
-	// TODO add debug for the result of the trajectory calculation from server
-	for _, point := range res.SwervePoints {
-		fmt.Println(point)
-	}
-
-	// plot.PlotSpline(path, "Path")
-	// plot.PlotSpline(path.Derivative(), "Path-Derivative")
-
-	// trajectory, _ := pathfinder.CreateTrajectoryPointArray(path, &chester, segments)
-	// pathfinder.LimitVelocityWithCentrifugalForce(trajectory, &chester)
-	// pathfinder.CalculateKinematics(trajectory, &chester)
-	// pathfinder.CalculateKinematicsReverse(trajectory, &chester)
-	// pathfinder.CalculateDtAndOmegaAfterReverse(trajectory)
-
-	// quantizedTrajectory := pathfinder.QuantizeTrajectory(trajectory, chester.CycleTime)
-
-	// swerveTrajectory := pathfinder.Get2DTrajectory(quantizedTrajectory, path)
-
-	// velTimeData := []vector.Vector{}
-	// velXTimeData := []vector.Vector{}
-	// velYTimeData := []vector.Vector{}
-	// velDistanceData := []vector.Vector{}
+	velTimeData := []vector.Vector{}
+	velXTimeData := []vector.Vector{}
+	velYTimeData := []vector.Vector{}
+	velDistanceData := []vector.Vector{}
 	// accTimeData := []vector.Vector{}
 
-	// headingTimeData := []vector.Vector{}
-	// headingDistanceData := []vector.Vector{}
+	headingTimeData := []vector.Vector{}
+	headingDistanceData := []vector.Vector{}
 
-	// omegaTimeData := []vector.Vector{}
-	// omegaDistanceData := []vector.Vector{}
+	omegaTimeData := []vector.Vector{}
+	omegaDistanceData := []vector.Vector{}
 
 	// positionXData := []vector.Vector{}
 	// positionYData := []vector.Vector{}
-	// positionData := []vector.Vector{}
+	positionData := []vector.Vector{}
 
-	// for _, trajectoryPoint := range quantizedTrajectory {
-	// 	velTimeData = append(velTimeData, vector.Vector{X: trajectoryPoint.Time, Y: trajectoryPoint.Velocity})
-	// 	accTimeData = append(accTimeData, vector.Vector{X: trajectoryPoint.Time, Y: trajectoryPoint.Acceleration})
-	// 	velDistanceData = append(velDistanceData, vector.Vector{X: trajectoryPoint.Distance, Y: trajectoryPoint.Velocity})
+	distance := 0.0
+	prevPosition := vector.NewFromRpcVector(firstSegment.Points[0].Position)
 
-	// 	headingTimeData = append(headingTimeData, vector.Vector{X: trajectoryPoint.Time, Y: trajectoryPoint.Heading})
-	// 	headingDistanceData = append(headingDistanceData, vector.Vector{X: trajectoryPoint.Distance, Y: trajectoryPoint.Heading})
+	for _, point := range res.SwervePoints {
+		currentPosition := vector.NewFromRpcVector(point.Position)
+		distance += currentPosition.Sub(prevPosition).Norm()
 
-	// 	omegaTimeData = append(omegaTimeData, vector.Vector{X: trajectoryPoint.Time, Y: trajectoryPoint.Omega})
-	// 	omegaDistanceData = append(omegaDistanceData, vector.Vector{X: trajectoryPoint.Distance, Y: trajectoryPoint.Omega})
+		// * Position
+		positionData = append(positionData, currentPosition)
 
-	// 	positionXData = append(positionXData, vector.Vector{X: trajectoryPoint.Time, Y: trajectoryPoint.Position.X})
-	// 	positionYData = append(positionYData, vector.Vector{X: trajectoryPoint.Time, Y: trajectoryPoint.Position.Y})
-	// 	positionData = append(positionData, trajectoryPoint.Position)
+		// * Velocity
+		currentVelocity := vector.NewFromRpcVector(point.Velocity)
 
-	// }
+		velNorm := currentVelocity.Norm()
 
-	// for _, point := range swerveTrajectory {
-	// 	velXTimeData = append(velXTimeData, vector.Vector{X: point.Time, Y: point.Velocity.X})
-	// 	velYTimeData = append(velYTimeData, vector.Vector{X: point.Time, Y: point.Velocity.Y})
-	// }
-	// plot.PlotScatter(positionData, "Position")
-	// plot.PlotScatter(positionXData, "PositionX-Time")
-	// plot.PlotScatter(positionYData, "PositionY-Time")
+		velTimeData = append(velTimeData, vector.Vector{X: float64(point.Time), Y: velNorm})
+		velXTimeData = append(velXTimeData, vector.Vector{X: float64(point.Time), Y: currentVelocity.X})
+		velYTimeData = append(velYTimeData, vector.Vector{X: float64(point.Time), Y: currentVelocity.Y})
+		velDistanceData = append(velDistanceData, vector.Vector{X: distance, Y: velNorm})
 
-	// plot.PlotScatter(velTimeData, "Velocity-Time")
-	// plot.PlotScatter(velXTimeData, "VelocityX-Time")
-	// plot.PlotScatter(velYTimeData, "VelocityY-Time")
-	// plot.PlotScatter(accTimeData, "Acceleration-Time")
-	// plot.PlotScatter(velDistanceData, "Velocity-Distance")
+		// * Heading
+		headingTimeData = append(headingTimeData, vector.Vector{X: float64(point.Time), Y: float64(point.Heading)})
+		headingDistanceData = append(headingDistanceData, vector.Vector{X: distance, Y: float64(point.Heading)})
 
-	// plot.PlotScatter(headingTimeData, "Heading-Time")
-	// plot.PlotScatter(headingDistanceData, "Heading-Distance")
+		// * Omega
+		omegaTimeData = append(omegaTimeData, vector.Vector{X: float64(point.Time), Y: float64(point.AngularVelocity)})
+		omegaDistanceData = append(omegaDistanceData, vector.Vector{X: distance, Y: float64(point.AngularVelocity)})
 
-	// plot.PlotScatter(omegaTimeData, "Omega-Time")
-	// plot.PlotScatter(omegaDistanceData, "Omega-Distance")
+		prevPosition = currentPosition
+	}
+
+	plot.PlotScatter(positionData, "Position")
+
+	plot.PlotScatter(velTimeData, "Velocity-Time")
+	plot.PlotScatter(velDistanceData, "Velocity-Distance")
+	plot.PlotScatter(velXTimeData, "VelocityX-Time")
+	plot.PlotScatter(velYTimeData, "VelocityY-Time")
+
+	plot.PlotScatter(headingTimeData, "Heading-Time")
+	plot.PlotScatter(headingDistanceData, "Heading-Distance")
+
+	plot.PlotScatter(omegaTimeData, "Omega-Time")
+	plot.PlotScatter(omegaDistanceData, "Omega-Distance")
 
 	http.ListenAndServe(":8081", nil)
 }
