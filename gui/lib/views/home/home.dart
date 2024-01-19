39b2f13f941e5ff7b77c6add147982b1f9bbc894
Itamar Schwartz <itamarschwartz10@gmail.com>
2024-01-19 20:34:16 +0200

    Allow removal of all tabs now that auto files are oer app instead of per tab

--- gui/lib/views/home/home.dart
+++ gui/lib/views/home/home.dart
@@ -198,17 +198,15 @@ class _HomePageState extends State<HomePage> {
                             ),
                           ),
                           onLongPress: () {
-                            if (index == props.currentTabIndex) {
-                              if (!props.changesSaved) {
-                                showAlertDialog(
-                                  context,
-                                  () => props.removeTab(props.currentTabIndex),
-                                  props.saveFile,
-                                  () {},
-                                );
-                              } else if (props.tabAmount > 1) {
-                                props.removeTab(props.currentTabIndex);
-                              }
+                            if (!props.changesSaved) {
+                              showAlertDialog(
+                                context,
+                                () => props.removeTab(props.currentTabIndex),
+                                props.saveFile,
+                                () {},
+                              );
+                            } else if (props.tabAmount > 1) {
+                              props.removeTab(props.currentTabIndex);
                             }
                           },
                           onPressed: () {
