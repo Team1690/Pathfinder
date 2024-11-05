52c3ecbfa80e87f0f90cb9aa425e30302325cfb0
IdoDePro <idozipori08@gmail.com>
2024-11-05 21:47:55 +0200

    added name and next funcs to drivetrain enum'

--- algorithm/drivetrain.go
+++ algorithm/drivetrain.go
@@ -3,6 +3,14 @@ package main
 type Drivetrain int
 
 const (
-	Swerve Drivetrain = iota + 1
+	Swerve Drivetrain = iota
 	Tank
 )
+
+func (drivetrain *Drivetrain) Name() string {
+	return [...]string{"Swerve", "Tank"}[*drivetrain]
+}
+
+func (drivetrain *Drivetrain) Next() Drivetrain {
+	return (*drivetrain + 1) % 2
+}
